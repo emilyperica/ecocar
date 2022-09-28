1832ee61018802c6c58decff12fa30e9c601bbcd
Emily Perica <74936925+emilyperica@users.noreply.github.com>
2022-09-28 16:42:04 -0400

    Update img_processing.py

--- img_processing.py
+++ img_processing.py
@@ -1,19 +1,46 @@
-import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+    
+    
+import rclpy
+from rclpy.node import Node
 
-# known pump geometry
-#  - units are pixels (of half-size image)
-PUMP_DIAMETER = 360
-PISTON_DIAMETER = 90
-PISTON_COUNT = 7
-
-def showImage(img):
-    cv2.imshow('image', img)
-    cv2.waitKey(1)
-
+class ImageSubscriber(Node):
+  """
+  Create an ImageSubscriber class, which is a subclass of the Node class.
+  """
+  def __init__(self):
+    """
+    Class constructor to set up the node
+    """
+    # Initiate the Node class's constructor and give it a name
+    super().__init__('image_subscriber')
+      
+    # Create the subscriber. This subscriber will receive an Image
+    # from the video_frames topic. The queue size is 10 messages.
+    self.subscription = self.create_subscription(
+      Image, 
+      'video_frames', 
+      self.listener_callback, 
+      10)
+    self.subscription # prevent unused variable warning
+      
+    # Used to convert between ROS and OpenCV images
+    self.br = CvBridge()
+   
+  def listener_callback(self, data):
+    """
+    Callback function.
+    """
+    # Display the message on the console
+    self.get_logger().info('Receiving video frame')
+ 
+    # Convert ROS Image message to OpenCV image
+    current_frame = self.br.imgmsg_to_cv2(data)
+    
+    
 def process_image(msg):
     try:
         # convert sensor_msgs/Image to OpenCV Image
@@ -32,16 +59,26 @@ def process_image(msg):
         print err
     # show results
     showImage(drawImg)
-
-def start_node():
-    rospy.init_node('detect_pump')
-    rospy.loginfo('detect_pump node started')
     
-    rospy.Subscriber("image", Image, process_image)
-    rospy.spin()
-
+    
+def main(args=None):
+  # Initialize the rclpy library
+  rclpy.init(args=args)
+  
+  # Create the node
+  image_subscriber = ImageSubscriber()
+  
+  # Spin the node so the callback function is called.
+  rclpy.spin(image_subscriber)
+  
+  # Destroy the node explicitly
+  # (optional - otherwise it will be done automatically
+  # when the garbage collector destroys the node object)
+  image_subscriber.destroy_node()
+  
+  # Shutdown the ROS client library for Python
+  rclpy.shutdown()
+  
 if __name__ == '__main__':
-    try:
-        start_node()
-    except rospy.ROSInterruptException:
-        pass
+  main()
+    
