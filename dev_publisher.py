8af31da7f71302d8f54efc695a8327ce5d219d76
Emily Perica <74936925+emilyperica@users.noreply.github.com>
2022-09-27 20:33:10 -0400

    Create dev_publisher.py

---
+++ dev_publisher.py
@@ -0,0 +1,9 @@
+import rospy
+from std_msgs.msg import String
+
+pub = rospy.Publisher('topic_name', String, queue_size=10)
+rospy.init_node('node_name')
+r = rospy.Rate(10) # 10hz
+while not rospy.is_shutdown():
+   pub.publish("hello world")
+   r.sleep()
