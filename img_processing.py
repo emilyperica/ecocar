6a899d5198772f54778b8f1d5bde997d3cf9f1d3
Emily Perica <74936925+emilyperica@users.noreply.github.com>
2022-09-27 20:45:28 -0400

    Create img_processing.py

---
+++ img_processing.py
@@ -0,0 +1,17 @@
+import rospy
+
+# known pump geometry
+#  - units are pixels (of half-size image)
+PUMP_DIAMETER = 360
+PISTON_DIAMETER = 90
+PISTON_COUNT = 7
+
+def start_node():
+    rospy.init_node('detect_pump')
+    rospy.loginfo('detect_pump node started')
+
+if __name__ == '__main__':
+    try:
+        start_node()
+    except rospy.ROSInterruptException:
+        pass
