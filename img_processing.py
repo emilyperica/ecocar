8f7f0e29eefbf4dc7cebb9933167ac3b881d8f83
Emily Perica <74936925+emilyperica@users.noreply.github.com>
2022-09-28 06:40:55 -0400

    Update img_processing.py

--- img_processing.py
+++ img_processing.py
@@ -1,5 +1,7 @@
 import rospy
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
 # known pump geometry
 #  - units are pixels (of half-size image)
@@ -7,9 +9,15 @@ PUMP_DIAMETER = 360
 PISTON_DIAMETER = 90
 PISTON_COUNT = 7
 
+def showImage(img):
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
+
 def process_image(msg):
     try:
-        pass
+        # convert sensor_msgs/Image to OpenCV Image
+        bridge = CvBridge()
+        orig = bridge.imgmsg_to_cv2(msg, "bgr8")
     except Excepttion as err:
         print err
 
